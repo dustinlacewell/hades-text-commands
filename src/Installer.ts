11ff962f8870f96c84c18c7a95ae780c5238b1b7
Lord-Valen <lord_valen@protonmail.com>
2022-06-22 15:56:02 -0400

    Rm hades tree

--- src/Installer.ts
+++
@@ -1,6 +0,0 @@
-import { Container } from "inversify";
-
-
-export abstract class Installer {
-    abstract install(container: Container): Promise<void>;
-}
---
+++ src/metadata/TextArgParserMeta.ts
@@ -0,0 +1,8 @@
+import { TextArgParser as TextParser } from "../parsers";
+import { Newable } from "hades";
+
+
+export class TextArgParserMeta {
+    type: Newable<TextParser>;
+    description?: string;
+}
